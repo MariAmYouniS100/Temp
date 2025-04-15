3b6190d89dac665e00110af362f566f3ef621dbe
youssef mohamed <youssef999975@gmail.com>
2025-04-15 23:24:36 +0200

    add student and instructor controller with views

--- Business logic layer/obj/Debug/net8.0/Business logic layer.AssemblyInfo.cs
+++ Business logic layer/obj/Debug/net8.0/Business logic layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business logic layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0047be2651864851974e0fb8d0be594c50093b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332beb2ecbbe0712debb592e828ade6bddd801a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business logic layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business logic layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Educational_Platform/obj/Debug/net8.0/Educational_Platform.AssemblyInfo.cs
+++ Educational_Platform/obj/Debug/net8.0/Educational_Platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0047be2651864851974e0fb8d0be594c50093b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332beb2ecbbe0712debb592e828ade6bddd801a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
